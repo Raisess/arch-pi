f4d9de68908f4a995a2aacdfe81453d52757e2a3
Raisess <raisechif@gmail.com>
2023-02-27 09:57:02 -0300

    feat: setup maestro serve

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -21,11 +21,12 @@ cd $TOOLS_PATH
 git clone https://github.com/Raisess/maestro && cd maestro && NO_SUDO=1 ./install.py && cd ..
 git clone https://github.com/Raisess/pingr && cd pingr && NO_SUDO=1 ./install.py && cd ..
 git clone https://github.com/Raisess/dbc && cd dbc && NO_SUDO=1 ./install.py && cd ..
-cd $HOME_PATH
 echo ">>> Done!"
 
 echo ">>> Setuping services..."
 cd $SERVICES_PATH
+maestro create maestro-serve "maestro serve"
+
 echo "[
   {
     \"name\": \"Some service\",
@@ -38,5 +39,6 @@ echo "[
     ]
   }
 ]" > services.json
-cd $HOME_PATH
 echo ">>> Done!"
+
+cd $HOME_PATH
