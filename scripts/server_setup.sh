3efc69a196ce0da0a90a6acf3b1f94b249ab1a11
Raisess <raisechif@gmail.com>
2023-02-28 18:46:56 -0300

    chore: add more tools

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -13,11 +13,17 @@ echo "ArchPI Server Setup | TOOLS_PATH: $TOOLS_PATH / SERVICES_PATH: $SERVICES_P
 mkdir -p $DOWNLOAD_PATH $TOOLS_PATH $SERVICES_PATH
 
 echo ">>> Installing dependencies..."
-pacman -S git python3 podman
+pacman -S git clangd python3 podman
 echo ">>> Done!"
 
 echo ">>> Setuping tools..."
 cd $TOOLS_PATH
+# Utils
+# CLI Creator is used by disk-mounter and other CLIs
+git clone https://github.com/Raisess/cli-creator && cd cli-creator && NO_SUDO=1 make && make install && cd ..
+
+# CLI's
+git clone https://github.com/Raisess/disk-mounter && cd disk-mounter && NO_SUDO=1 make && make install && cd ..
 git clone https://github.com/Raisess/maestro && cd maestro && NO_SUDO=1 ./install.py && cd ..
 git clone https://github.com/Raisess/pingr && cd pingr && NO_SUDO=1 ./install.py && cd ..
 git clone https://github.com/Raisess/dbc && cd dbc && NO_SUDO=1 ./install.py && cd ..
