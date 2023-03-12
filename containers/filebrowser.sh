9490566d46323e50baaffd537c7270eda901f322
Raisess <raisechif@gmail.com>
2023-03-12 13:25:09 -0300

    chore: small improvements

--- containers/filebrowser.sh
+++ containers/filebrowser.sh
@@ -1,19 +1,22 @@
 #! /usr/bin/env sh
 
+CONTAINER_NAME=filebrowser
 APPDATA_PATH=/media/sda/.appdata
+CONTAINER_APPDATA_PATH=$APPDATA_PATH/$CONTAINER_NAME
+
 DATA_PATH=/media/sda
 
-mkdir -p $APPDATA_PATH/filebrowser
-touch $APPDATA_PATH/filebrowser/settings.json
-touch $APPDATA_PATH/filebrowser/filebrowser.db
+mkdir -p $CONTAINER_APPDATA_PATH
+touch $CONTAINER_APPDATA_PATH/settings.json
+touch $CONTAINER_APPDATA_PATH/filebrowser.db
 
-podman create --name filebrowser \
-	-v $APPDATA_PATH/filebrowser/settings.json:/config/settings.json \
-	-v $APPDATA_PATH/filebrowser/filebrowser.db:/database/filebrowser.db \
+podman create --name $CONTAINER_NAME \
+	-v $CONTAINER_APPDATA_PATH/settings.json:/config/settings.json \
+	-v $CONTAINER_APPDATA_PATH/filebrowser.db:/database/filebrowser.db \
 	-v $DATA_PATH:/srv \
 	-e PUID=$(id -u) \
 	-e PGID=$(id -g) \
 	-p 1234:80 \
 	docker.io/filebrowser/filebrowser:latest
 
-maestro create filebrowser "podman start filebrowser --attach"
+maestro create $CONTAINER_NAME "podman start $CONTAINER_NAME --attach"
--- containers/jellyfin.sh
+++ containers/jellyfin.sh
@@ -1,18 +1,19 @@
 #! /usr/bin/env sh
 
+CONTAINER_NAME=jellyfin
 APPDATA_PATH=/media/sda/.appdata
+CONTAINER_APPDATA_PATH=$APPDATA_PATH/$CONTAINER_NAME
+
 DATA_PATH=/media/sda/media/video
 
-mkdir -p $APPDATA_PATH/jellyfin
-mkdir $APPDATA_PATH/jellyfin/cache
-mkdir $APPDATA_PATH/jellyfin/config
+mkdir -p $CONTAINER_APPDATA_PATH/{cache,config}
 
-podman create --name jellyfin \
+podman create --name $CONTAINER_NAME \
 	--user $(id -u):$(id -g) \
-	-v $APPDATA_PATH/jellyfin/cache:/cache \
-	-v $APPDATA_PATH/jellyfin/config:/config \
+	-v $CONTAINER_APPDATA_PATH/cache:/cache \
+	-v $CONTAINER_APPDATA_PATH/config:/config \
 	-p 8096:8096/tcp \
 	--mount type=bind,source=$DATA_PATH,destination=/media,ro=true,relabel=private \
 	docker.io/jellyfin/jellyfin:latest
 
-maestro create jellyfin "podman start jellyfin --attach"
+maestro create $CONTAINER_NAME "podman start $CONTAINER_NAME --attach"
