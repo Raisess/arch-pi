f4316eb386f1d8d6c70b6d9f2ae9fb12d12f3cc0
Raisess <raisechif@gmail.com>
2023-04-01 10:55:29 -0300

    chore: update `update_maestro` script

--- scripts/update_maestro.sh
+++ scripts/update_maestro.sh
@@ -2,17 +2,17 @@
 
 echo "Updating maestro..."
 
-PASSWORD_FILE_PATH=/usr/local/etc/maestro/password.json
+CONF_FILE_PATH=/usr/local/etc/maestro/conf.json
 
 maestro kill maestro-serve
 cd /tmp
-cp $PASSWORD_FILE_PATH .
+cp $CONF_FILE_PATH .
 git clone https://github.com/Raisess/maestro
 cd ./maestro
 NO_SUDO=1 ./install.py
 cd ..
 rm -rf ./maestro
-cp password.json $PASSWORD_FILE_PATH
+cp conf.json $CONF_FILE_PATH
 maestro run maestro-serve
 
 echo "Done!"
